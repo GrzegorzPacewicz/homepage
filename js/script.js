4754fbfd109aca5f34ef9664817e794b9b109d01
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2021-06-30 22:39:23 +0200

    JS script changed. "Let" changed to "const".

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-console.log("Yo!")
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName");
+{
+    const welcome = () => {
+        console.log("Cześć! Fajnie, że tu zajrzałeś!")   
+       }
+    welcome();   
+
+    const button = document.querySelector(".js-button");
+    const body = document.querySelector(".js-body");
+    const themeName = document.querySelector(".js-themeName");
 
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
+    button.addEventListener("click", () => {
+        body.classList.toggle("body--dark");
 
-    themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-});
-
+        themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+    });
+}
